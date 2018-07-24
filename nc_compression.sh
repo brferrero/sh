02138b68eaf46b2d1c2c4179754e308fcfb7b72b
brferrero <brferrero@gmail.com>
2018-07-24 15:29:06 -0300

    explicit deflate opt

--- nc_compression.sh
+++ nc_compression.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 #
-# compress a list of netcdf files using minimal deflation (~60% compression)
+# compress a list of netcdf files using minimal deflation {'1' : ~60% compression}
 #
 
 for f in $(ls *.nc)
 do
     echo "Compressing " ${f} "..." 
-    ncks -4 -L 1 -O ${f} ${f}
+    ncks -4 --deflate 1 -O ${f} ${f}
 done
