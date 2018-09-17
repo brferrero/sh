d068590a1a27d050d6492f37a806ea0590424593
brferrero <brferrero@gmail.com>
2018-09-17 19:34:30 -0300

    list only matching files

--- nc_compression.sh
+++ nc_compression.sh
@@ -3,7 +3,8 @@
 # compress a list of netcdf files using minimal deflation {'1' : ~60% compression}
 #
 
-for f in $(ls *.nc)
+MATCH=$1
+for f in $(ls *${MATCH}*.nc)
 do
     echo "Compressing " ${f} "..." 
     ncks -4 --deflate 1 -O ${f} ${f}
